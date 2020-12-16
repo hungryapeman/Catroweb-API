d86952e73eb021155515701b5400d215f06fa701
Maximilian Fazeli <m.fazeli@student.tugraz.at>
2020-12-16 19:10:22 +0100

    test

--- Tests/AppKernel.php
+++ Tests/AppKernel.php
@@ -3,20 +3,22 @@
 namespace OpenAPI\Server\Tests;
 
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
-use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
-  public function registerBundles()
-  {
-    return [
-      new FrameworkBundle(),
-    ];
-  }
+    public function registerBundles()
+    {
+        $bundles = array(
+            new FrameworkBundle()
+        );
 
-  public function registerContainerConfiguration(LoaderInterface $loader)
-  {
-    $loader->load(__DIR__.'/test_config.yml');
-  }
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/test_config.yml');
+    }
 }
